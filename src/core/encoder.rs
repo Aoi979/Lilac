c77495f0c21c19244df3e4650ad0d0940239ad97
aoi <aoi2119721853@gmail.com>
2024-06-05 12:49:09 +0800

    original commit

--- src/core/encoder.rs
+++ src/core/encoder.rs
@@ -1,10 +1,4 @@
 use crate::core::buffer::{Buffer, Encoder};
 
-pub struct BaseEncoder(Buffer);
-
-impl Encoder for BaseEncoder{
-    fn encode(&self, buffer: &Buffer, offset: u32) -> Result<Vec<u8>, Err()> {
 
 
-    }
-}
